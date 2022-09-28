41374fda689b4bde42a3c0a794e49adc4341462c
MattDog222 <wilder.mdog@gmail.com>
2022-09-27 23:53:42 -0500

    finished question 6

---
+++ seconds.c
@@ -0,0 +1,111 @@
+/**
+ * seconds.c
+ *
+ * Kernel module that communicates with /proc file system.
+ *
+ * The makefile must be modified to compile this program.
+ * Change the line "simple.o" to "hello.o"
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#include <asm/param.h>
+#include <linux/jiffies.h> // pronounced "Giffies"
+
+#define BUFFER_SIZE 128
+
+#define PROC_NAME "seconds"
+
+
+unsigned long gifsAtInit;
+
+
+/**
+ * Function prototypes
+ */
+static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+    	.owner = THIS_MODULE,
+    	.read = proc_read,
+};
+
+
+/* This function is called when the module is loaded. */
+static int proc_init(void)
+{
+    gifsAtInit = jiffies;
+
+    	// creates the /proc/seconds entry
+    	// the following function call is a wrapper for
+    	// proc_create_data() passing NULL as the last argument
+    	proc_create(PROC_NAME, 0, NULL, &proc_ops);
+
+    	printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+
+    return 0;
+}
+
+/* This function is called when the module is removed. */
+static void proc_exit(void) {
+
+    	// removes the /proc/seconds entry
+    	remove_proc_entry(PROC_NAME, NULL);
+
+    	printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+/**
+ * This function is called each time the /proc/seconds is read.
+ *
+ * This function is called repeatedly until it returns 0, so
+ * there must be logic that ensures it ultimately returns 0
+ * once it has collected the data that is to go into the
+ * corresponding /proc file.
+ *
+ * params:
+ *
+ * file:
+ * buf: buffer in user space
+ * count:
+ * pos:
+ */
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+    	int rv = 0;
+    	char buffer[BUFFER_SIZE];
+    	static int completed = 0;
+    long secondsElapsed;
+
+    	if (completed) {
+            	completed = 0;
+            	return 0;
+    	}
+
+    	completed = 1;
+
+
+    secondsElapsed = (jiffies - gifsAtInit) / HZ;
+
+    	rv = sprintf(buffer, "Seconds since module initialization: %lu\n", secondsElapsed);
+
+    	// copies the contents of buffer to userspace usr_buf
+    	copy_to_user(usr_buf, buffer, rv);
+
+    	return rv;
+}
+
+
+/* Macros for registering module entry and exit points. */
+module_init( proc_init );
+module_exit( proc_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Seconds Module");
+MODULE_AUTHOR("SGG");
